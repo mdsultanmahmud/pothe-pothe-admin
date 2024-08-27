c30e111105a26f647396ae16efaee99b750cdf05
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2024-08-27 13:40:02 +0600

    feat: setup react project for admin dashboard

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div>
+      <h1>hello world</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+        molestias doloremque quasi accusamus facere quam itaque repellat
+        voluptatem corrupti accusantium? Harum sequi ut nam recusandae rerum
+        autem dicta, consectetur optio blanditiis incidunt similique pariatur
+        quam libero fugit doloremque veniam neque amet quas assumenda ipsam
+        nesciunt! Sunt a dignissimos itaque doloremque.
+      </p>
+      <br />
+      <hr />
+      <br />
+      <a href="#">this is link</a>
+    </div>
+  );
+}
+
+export default App;
